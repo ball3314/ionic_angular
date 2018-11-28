e17c41cbb5c4f1ea2566c3bc67e6f95896555cb9
daniel.yang <daniel.yang@tech-trans.com>
2018-11-28 22:10:04 +0800

    第一次初始化，ionic-angular類型的專案

---
+++ src/pages/xy/xy.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { XyPage } from './xy';
+
+@NgModule({
+  declarations: [
+    XyPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(XyPage),
+  ],
+})
+export class XyPageModule {}
